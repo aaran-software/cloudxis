c5a4b77e13e2c4e29a55499ba5ca9cf169b88398
SUNDAR <sundar@codexsun.com>
2026-01-13 10:15:49 +0530

    #29 added tm software

--- docker/cloud/init_frappe.py
+++ docker/cloud/init_frappe.py
@@ -4,12 +4,12 @@ import os
 import subprocess
 
 FRAPPE_BRANCH = "develop"
-SITE_NAME = "dev.software.com"
+SITE_NAME = "tm.software.com"
 ADMIN_PASS = "admin"
 DB_USER = "root"
 DB_PASS = "DbPass1@@"
 DB_HOST = "mariadb"
-DB_NAME = "dev_software_db"
+DB_NAME = "tm_software_db"
 BENCH_DIR = "/home/devops/cloud/frappe-bench"
 SUPERVISOR_CONF = "/etc/supervisor/conf.d/frappe.conf"
 EMAIL = f"admin@{SITE_NAME}"
